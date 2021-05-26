ebc46de4ee9b4673ffd3b0576fe654701ed992ca
Davi <davi.freitas@useall.com.br>
2021-05-26 13:18:13 -0300

    JWT login auth

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,4 +1,6 @@
 const Usuario = require('../models/usuarios.model');
+const jwt = require('jsonwebtoken');
+const secret = "meusegredo";
 
 module.exports = {
     async index (request, response) {
@@ -45,5 +47,24 @@ module.exports = {
         const user = await Usuario.findOneAndUpdate({_id}, data, { new: true}); // Passando new: true para trazer os dados atualizado
 
         response.json(user);
+    },
+
+    async login(request, response) {
+        const { email, senha } = request.body;
+        Usuario.findOne({ email_usuario: email, tipo_usuario: 1 }, function (err, user) {
+            if(err) {
+                console.log(err);
+                response.status(200).json({ erro: "Erro no servidor. Por favor, tente novamente" })
+            } else if (!user) {
+                response.status(200).json({ status: 2, erro: 'Email ou senha não conferem'});
+            } else {
+                const payload = { email };
+                const token = jwt.sign(payload, secret, {
+                    expiresIn: '24h'
+                });
+                response.cookie('token', token, { httpOnly: true });
+                response.status(200).json({ status: 1, auth: true, token: token, id_client: user._id, user_name: user.nome_usuario });
+            }
+        })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ routes.get('/', Usuario.index);
 
 // Rotas de Usuários
 routes.post('/api/usuarios', Usuario.create);
+routes.post('/api/usuarios/login', Usuario.login);
 routes.get('/api/usuarios', Usuario.index);
 routes.get('/api/usuarios/details/:_id', Usuario.details);
 routes.delete('/api/usuarios/:_id', Usuario.delete);
